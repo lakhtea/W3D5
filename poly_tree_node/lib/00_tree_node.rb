9ebc770c9c4505d2f7dd569797de750ccf90a71c
Lakhte <lakhteagha@gmail.com>
2021-01-15 15:53:14 -0500

    added dfs

--- poly_tree_node/lib/00_tree_node.rb
+++ poly_tree_node/lib/00_tree_node.rb
@@ -8,17 +8,38 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        current_parent = self.parent
-        current_parent.children.each { |child| current_parent.children.delete(self)  }
+        if self.parent
+            current_parent = self.parent
+            current_parent.children.each { |child| current_parent.children.delete(self)  }
+        end
         @parent = node
-        node.children << self unless node.children.include?(self) || node.nil?
+        node.children << self unless node.nil? || node.children.include?(self)
     end
 
     def add_child(node)
-        self.parent = node
+        node.parent = self
+    end
+
+    def remove_child(node)
+        if node.parent.nil?
+            raise "Node is not a child"
+        end
+        node.parent = nil
+    end
+
+      def dfs(target)
+        return self if self.value == target
+
+        self.children.each do |child| 
+            result = child.dfs(target)
+            return result if !result.nil?
+        end
+
+        nil
     end
 
 end
+
 
 # node1 = PolyTreeNode.new(value1)
 # node2 = PolyTreeNode.new(value2) # parent
